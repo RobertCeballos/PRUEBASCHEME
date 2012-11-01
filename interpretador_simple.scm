44d2885cf89be292b248d0232140fb98dc7599ed
juandapp <juandavid162@gmail.com>
2012-11-01 13:32:26 -0500

    mi función jd

--- interpretador_simple.scm
+++ interpretador_simple.scm
@@ -256,5 +256,17 @@
 ;                         grammar-simple-interpreter)))
 ;(read-eval-print)
 
+;;esta es mi función 
+(define apply-env
+  (lambda (env sym)
+    (cases environment env
+      (empty-env-record ()
+                        (eopl:error 'apply-env "No binding for ~s" sym))
+      (extended-env-record (syms vals env)
+                           (let ((pos (list-find-position sym syms)))
+                             (if (number? pos)
+                                 (list-ref vals pos)
+                                 (apply-env env sym)))))))
+
 
 (read-eval-print)
